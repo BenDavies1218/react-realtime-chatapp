01da8393fceee6d2f70816b81dae9c5ce766271c
BenDavies1218 <14972@coderacademy.edu.au>
2024-05-09 10:54:29 +1000

    feat: new favicon

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -102,7 +102,7 @@ const Login = () => {
       {login ? (
         <div className="login">
           <div className="item">
-            <h2>Welcome back,</h2>
+            <h2>Welcome back</h2>
             <form onSubmit={handleLogin}>
               <input type="text" placeholder="Email" name="email" />
               <input type="password" placeholder="Password" name="password" />
